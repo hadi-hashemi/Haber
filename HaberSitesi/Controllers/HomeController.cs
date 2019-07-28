216f78d9e156b5ab4d236e300ce5ee8dc4708af9
Hadi Hashemi <hadi-hashemi@hotmail.com>
2019-07-28 18:20:24 +0300

    Uygulama

---
+++ HaberSitesi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HaberSitesi.Models.DataBase;
+
+namespace HaberSitesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        HaberSitesiDBEntities HaberSitesiDB = new HaberSitesiDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<tbl_Haber> Haber = HaberSitesiDB.tbl_Haber.ToList();
+            return View(Haber);
+        }
+    }
+}
---
+++ HaberSitesi/Controllers/KullaniciController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaberSitesi.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        // GET: Kullanici
+        public ActionResult Giris()
+        {
+            return View();
+        }
+
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+    }
+}
